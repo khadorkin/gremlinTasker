8f6ac55a137389bfdc6f4e1275b0a38405311cae
Matthew Harmon <harmon.matthewd@gmail.com>
2016-03-04 12:52:20 -0600

    refactor: Cleaned up some uneeded code
    Removed most of the copy pasta when trying to get the git_changelog running.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,6 @@ var webpack = require('webpack');
 var uglify = new webpack.optimize.UglifyJsPlugin();
 
 module.exports = function(grunt) {
-  // require('load-grunt-tasks')(grunt);
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -39,39 +38,19 @@ module.exports = function(grunt) {
           file: './CHANGELOG.md',
           repo_url: 'https://github.com/mattdharmon/gremlinTasker',
           app_name : 'Gremlin Tasker changelog',
+          grep_commits: '^fix|^feat|^docs|^refactor|^chore|BREAKING',
           debug: true,
           tag: false
         }
-      },
-      extended: {
-        options: {
-          repo_url: 'https://github.com/mattdharmon/gremlinTasker',
-          app_name : 'Gremlin Tasker extended',
-          file : './EXTENDEDCHANGELOG.md',
-          grep_commits: '^fix|^feat|^docs|^refactor|^chore|BREAKING',
-          tag : false //False for commits since the beggining
-        }
-      },
-      // Do this when I start tagging releases.
-      // fromCertainTag: {
-      //   options: {
-      //     repo_url: 'https://github.com/mattdharmon/gremlinTasker',
-      //     app_name : 'Gremlin Tasker changelog',
-      //     file : 'tags/certainTag.md',
-      //     tag : 'v0.0.1'
-      //   }
-      // }
+      }
     }
   });
 
 
   grunt.loadNpmTasks('grunt-webpack');
   grunt.loadNpmTasks('git-changelog');
-  // grunt.loadNpmTasks('grunt-changelog');
   grunt.loadNpmTasks('grunt-shell');
 
   // Default task(s).
-
   grunt.registerTask('default', ['webpack', 'shell:migrate']);
-  // grunt.registerTask('changelog', ['git_changelog:minimal', 'git_changelog:extended']);
 };
